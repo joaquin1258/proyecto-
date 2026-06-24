81496d1cd581d7ab4193060ef5718aa666a0beee
denissegodoyr <denisse.godoy.r@mail.pucv.cl>
2026-06-24 19:43:33 -0400

    Implementación función de búsqueda de contraseñas en base usuario y cuenta, correción de mapas y entidades

--- prueba.c
+++ prueba.c
@@ -6,10 +6,23 @@
 #include "tdas/list.h"
 #include "tdas/map.h"
 
+
+typedef struct {
+    char nombrePerfil[50] ;
+    char salt[10] ;
+} perfil ;
+
 typedef struct {
     char nombre[50];
     Map *mapa_claves;
 } usuario;
+
+typedef struct {
+    char nombreCuenta[50] ;
+    char password[20] ;
+} cuenta ;
+
+
 
 int is_equal_str(void *key1, void *key2) {
   return strcmp((char *)key1, (char *)key2) == 0;
@@ -131,6 +144,77 @@ void crearCuenta(Map *cuentas) {
     }
 }
 
+void buscarContra(Map *nombresUsuarios) {
+    char opcion ;
+    printf("¿Desea buscar por nombre de usuario o cuenta?") ;
+    printf("1. Usuario\n2. Cuenta\n") ;
+    scanf("%c", &opcion) ;
+
+    if (opcion=='1') {
+        char user[50] ;
+        printf("Ingrese nombre de usuario: ") ;
+        scanf("%49s", user) ;
+        MapPair *usuario=map_search(nombresUsuarios, user) ;
+        if (usuario==NULL) {
+            printf("Nombre de usuario no registrado, intente nuevamente.\n") ;
+        }
+        else {
+            printf("Cuentas asociadas al nombre de usuario: \n") ;
+            Map *mapaServicios=usuario->value ;
+            MapPair *cuenta=map_first(mapaServicios) ;
+            while (cuenta!=NULL) {
+                printf("%s\n", cuenta->key) ;
+                cuenta=map_next(mapaServicios) ;
+            }
+            printf("¿A cuál cuenta desearía ver la clave?\n") ;
+            char servicio[50] ;
+            scanf("%s", servicio) ;
+            MapPair *par=map_search(mapaServicios, servicio) ;
+            if (par!=NULL) {
+                char clave[20] ;
+                strcpy(clave, par->value) ;
+                // funcion de cifrado para descifrar
+                printf("Nombre de usuario: %s\n", user) ;
+                printf("Cuenta: %s\n", servicio) ;
+                printf("Clave: %s\n", clave) ;
+            }
+        }
+    }
+    if (opcion=='2') {
+        char cuenta[50] ;
+        unsigned short aux=0 ;
+        printf("Ingrese cuenta: ") ;
+        scanf("%s, cuenta") ;
+        
+        MapPair *usuarios=map_first(nombresUsuarios) ;
+        while (usuarios!=NULL) {
+            Map *servicios=usuarios->value ;
+            MapPair *servicio=map_search(servicios, cuenta) ;
+            if (servicio==NULL) {
+                aux=1 ;
+            }
+            else {
+                char clave[20] ;
+                strcpy(clave, servicio->value) ;
+                //funcion cifrado para descifrar
+                printf("Nombre de usuario: %s\n", usuarios->key) ;
+                printf("Cuenta: %s\n", servicio->key) ;
+                printf("Clave de la cuenta: %s", clave) ;
+                aux=0 ;
+                break ;
+            }
+            usuarios=map_next(nombresUsuarios) ;
+        }
+        if (aux==1) {
+            printf("Cuenta no encontrada.\n") ;
+        }
+    }
+    else {
+        printf("ERROR, opción inválida.\n") ;
+    }
+}
+
+
 void verificarClave(char *clave, List *lista_clavesMasUsadas) {
     int largo = strlen(clave);
     int mayuscula = 0; 
@@ -175,6 +259,8 @@ int main(){
     printf("Bienvenido al gestor de claves\n");
     printf("Cargando claves mas usadas...\n");
 
+    Map *mapaUsuarios=map_create(is_equal_str) ;
+
     List *lista_clavesMasUsadas = cargarClavesMasUsadas(); 
     Map *mapa_usuarios = map_create(is_equal_str);
     int resultado = 1;
@@ -214,7 +300,7 @@ int main(){
 
         switch (opcion) {
             case '1':
-                crearCuenta(mapa_usuarios);
+                crearCuenta(mapaUsuarios);
                 break;
             case '2':
                 printf("Opcion 2 seleccionada\n");
